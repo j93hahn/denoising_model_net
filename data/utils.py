7d06a59d41b818464c1179c6e92abd818b21c275
Joshua Ahn <joshuajahn2001@gmail.com>
2024-09-28 16:21:57 -0400

    rename

--- data/utils.py
+++ data/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 import open3d as o3d
 
@@ -13,16 +15,36 @@ class OFFObject:
         mesh.triangles = o3d.utility.Vector3iVector(self.faces)
         o3d.visualization.draw_geometries([mesh])
 
+    @staticmethod
+    def read_off_file(file: str) -> OFFObject:
+        """
+        https://segeval.cs.princeton.edu/public/off_format.html
+        """
+        assert file.endswith('off'), 'File must be a .off file'
+
+        with open(file, 'r') as f:
+            lines = f.readlines()
+
+        assert lines[0].strip() == 'OFF', 'First line must be "OFF"'
+
+        num_vertices, num_faces, _ = map(int, lines[1].split())
+
+        vertices = []
+        for line in lines[2:2 + num_vertices]:
+            vertices.append(list(map(float, line.split())))
+
+        faces = []
+        for line in lines[2 + num_vertices:]:
+            faces.append(list(map(int, line.split()))[1:])
+
+        return OFFObject(vertices, faces)
+
 
 def dir_vine(data_dir: str):
     return Path(dir) / 'vine'
 
 
-"""
-https://segeval.cs.princeton.edu/public/off_format.html
-"""
-def read_off_file(file: str):
-    assert file.endswith('off'), 'File must be a .off file'
-
-    with open(file, 'r') as f:
-        lines = f.readlines()
+if __name__ == '__main__':
+    file = 'data/example.off'
+    obj = OFFObject.read_off_file(file)
+    obj.viz()
